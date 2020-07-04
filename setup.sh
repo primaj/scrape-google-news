ae2143b2c85d219be3e528b3d971637f7b21693b
John Primavesi <johnprimavesi@gmail.com>
2020-07-04 18:57:29 +0100

    remove unnecessary files

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 mkdir -p ~/.streamlit/
 
-
 echo "[general]\nemail = \"johnprimavesi@gmail.com\""  > ~/.streamlit/credentials.toml
 
 echo "[server]\nheadless = true\nenableCORS = false\nport = $PORT" > ~/.streamlit/config.toml
