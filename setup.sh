a1503a9dae1dd6ad40411b909adb7c2a1be36ebf
primaj <40952801+primaj@users.noreply.github.com>
2020-11-13 16:49:42 +0000

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p ~/.streamlit/
-
-echo "[general]\nemail = \"johnprimavesi@gmail.com\""  > ~/.streamlit/credentials.toml
-
-echo "[server]\nheadless = true\nenableCORS = false\nport = $PORT" > ~/.streamlit/config.toml
-
-
